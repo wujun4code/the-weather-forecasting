01c3ec385d09a04d900371d47b5a4c2d8a583068
agus-3rd-yoga <agus.3rd.yoga@gmail.com>
2023-12-21 09:39:06 +0700

    Add API key

--- src/api/OpenWeatherService.js
+++ src/api/OpenWeatherService.js
@@ -1,7 +1,7 @@
 const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
 const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
-const WEATHER_API_KEY = 'Your API KEY';
+const WEATHER_API_KEY = '09ad97c817d43aa142fc57899254a56b';
 
 const GEO_API_OPTIONS = {
   method: 'GET',
