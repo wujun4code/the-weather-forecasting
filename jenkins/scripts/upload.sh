41450fa24c9319a15813d5f5b6c901258de4560d
agus-3rd-yoga <agus.3rd.yoga@gmail.com>
2023-12-26 21:04:01 +0700

    fix upload #9

--- jenkins/scripts/upload.sh
+++ jenkins/scripts/upload.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 
 # INSTALL REQUIRED PACKAGES
-npm ci
+#npm ci
 #npm run build
 
 # CONFIGURE NPM SETTINGS
-echo "@agus3rdyoga:registry=https://registry.npmjs.org" > .npmrc
-echo "unsafe-perm=true"
-echo "https://registry.npmjs.org/:_password:^7(!:t9Vb$BJpkV" >> .npmrc
-echo "https://registry.npmjs.org/:username:agus3rdyoga" >> .npmrc
-echo "https://registry.npmjs.org/:email:agus.3rd.yoga@gmail.com" >> .npmrc
-echo "https://registry.npmjs.org/:always-auth=false" >> .npmrc
+#echo "@agus3rdyoga:registry=https://registry.npmjs.org" > .npmrc
+#echo "unsafe-perm=true"
+#echo "https://registry.npmjs.org/:_password:^7(!:t9Vb$BJpkV" >> .npmrc
+#echo "https://registry.npmjs.org/:username:agus3rdyoga" >> .npmrc
+#echo "https://registry.npmjs.org/:email:agus.3rd.yoga@gmail.com" >> .npmrc
+#echo "https://registry.npmjs.org/:always-auth=false" >> .npmrc
 
 # Publish!
-npm set //registry.npmjs.org/h24z:_authToken=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
-npm adduser
-npm publish -access public
+npm set //registry.npmjs.org/:_authToken=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
+npm publish --access public
