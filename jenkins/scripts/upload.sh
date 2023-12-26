457f4ea6928351ccf7d25ea8ec3a4f2118772ddb
agus-3rd-yoga <agus.3rd.yoga@gmail.com>
2023-12-26 21:16:03 +0700

    fix upload #11

--- jenkins/scripts/upload.sh
+++ jenkins/scripts/upload.sh
@@ -5,15 +5,16 @@
 #npm run build
 
 # CONFIGURE NPM SETTINGS
-echo "@agus3rdyoga:registry=https://registry.npmjs.org" > .npmrc
-echo "unsafe-perm=true"
-echo "https://registry.npmjs.org/:_password:^7(!:t9Vb$BJpkV" >> .npmrc
+echo "@h24z:registry=https://registry.npmjs.org/" > .npmrc
+#echo "@agus3rdyoga:registry=https://registry.npmjs.org" > .npmrc
+#echo "unsafe-perm=true"
+#echo "https://registry.npmjs.org/:_password:^7(!:t9Vb$BJpkV" >> .npmrc
 #echo "https://registry.npmjs.org/:username:agus3rdyoga" >> .npmrc
 #echo "https://registry.npmjs.org/:email:agus.3rd.yoga@gmail.com" >> .npmrc
 #echo "https://registry.npmjs.org/:always-auth=false" >> .npmrc
 
 # Publish!
-npm config set registry http://registry.npmjs.org
-npm login
+#npm config set registry http://registry.npmjs.org
+#npm login
 #npm set //registry.npmjs.org/:_authToken=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
-npm publish --access public
+npm publish --registry https://registry.npmjs.org
