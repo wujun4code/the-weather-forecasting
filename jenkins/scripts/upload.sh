a679aac895b46f8d9e6c55ced3b1f286923e5efc
agus-3rd-yoga <agus.3rd.yoga@gmail.com>
2023-12-26 18:49:13 +0700

    Fix upload #2

--- jenkins/scripts/upload.sh
+++ jenkins/scripts/upload.sh
@@ -5,7 +5,7 @@ npm ci
 npm run build
 
 # CONFIGURE NPM SETTINGS
-echo "@agus3rdyoga:registry=https://registry.npmjs.org" >> .npmrc
+echo "@agus3rdyoga:registry=https://registry.npmjs.org" > .npmrc
 echo "unsafe-perm=true"
 echo "https://registry.npmjs.org/:_password:^7(!:t9Vb$BJpkV" >> .npmrc
 echo "https://registry.npmjs.org/:username:agus3rdyoga" >> .npmrc
@@ -13,4 +13,5 @@ echo "https://registry.npmjs.org/:email:agus.3rd.yoga@gmail.com" >> .npmrc
 echo "https://registry.npmjs.org/:always-auth=false" >> .npmrc
 
 # Publish!
+npm login
 npm publish
